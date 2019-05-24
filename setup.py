259b9c8dfdcb5d3a2ee42fab9f81fc911bd688d0
Osiris Kohmet <k.j.hirner.wisdom@gmail.com>
2019-05-23 23:03:03 -0700

    updates create a more modular structure

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-     name='pyhardware',  
+     name='phw',  
      version='0.1',
      #packages=['hw'],
      author="Karl Josef Wisdom",
